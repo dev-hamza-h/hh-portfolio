e211897a128edef002b18df22df6b3f92995a7d7
Hamza Hassan <hk5732065@gmail.com>
2024-11-10 04:58:01 +0500

    Initial commit

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,9 +1,8 @@
 import Link from "next/link";
 
-
 export default function Footer() {
     return (
-      <footer className="bg-blue-950 text-white py-6">
+      <footer className="bg-blue-950 text-white py-6 border-t-2 border-cyan-500 shadow-[0_4px_20px_rgba(0,255,255,0.6)] shadow-cyan-500">
         <div className="container mx-auto text-center">
           <p className="text-sm mb-2">
             &copy; {new Date().getFullYear()} Hamza Hassan. All Rights Reserved.

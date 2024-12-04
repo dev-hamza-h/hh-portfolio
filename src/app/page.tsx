9b61f27e9e8f59175dd823752813ac29d7ac7925
Hamza Hassan <hk5732065@gmail.com>
2024-12-04 17:53:16 +0500

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,9 @@
 import Hero from "@/components/layout/HeroSection";
-import About from "@/app/about/page";
-import Skills from "@/app/skills/page";
-import Education from "@/app/education/page"
-import Services from "@/app/services/page"
-import Contact from "@/app/contact/page"
 
 export default function Home(){
     return(
         <div>
             <Hero/>
-            <About/>
-            <Skills/>
-            <Education/>
-            <Services/>
-            <Contact/>
         </div>
     )
 }
